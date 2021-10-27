aa1312f10f88661a168dc0a5913b7b7e3e61c743
rakinplaban <rakinshahriar54@gmail.com>
2021-10-28 00:21:31 +0600

    created posts db.

--- network/admin.py
+++ network/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import User
+from .models import User, NewPost
 # Register your models here.
 
 class User_display(admin.ModelAdmin):
     list_display = ("id","username","password")
 
-admin.site.register(User,)
+class Post_display(admin.ModelAdmin):
+    list_display = ("id","post","user","timestamp")
+
+admin.site.register(User,User_display)
+admin.site.register(NewPost,Post_display)
--- network/models.py
+++ network/models.py
@@ -4,3 +4,8 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class NewPost(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    post = models.TextField()
+    timestamp = models.DateTimeField()
