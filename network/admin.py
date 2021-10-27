c42c0e806a63619cd6b2aa6531f956d5d1d3b8ec
rakinplaban <rakinshahriar54@gmail.com>
2021-10-27 14:13:49 +0600

    CSS desigh.

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import User
 # Register your models here.
+
+class User_display(admin.ModelAdmin):
+    list_display = ("id","username","password")
+
+admin.site.register(User,)
