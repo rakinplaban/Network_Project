e55eb3855639131d5992199cd03121bd1b76a0e8
rakinplaban <rakinshahriar54@gmail.com>
2021-12-01 14:02:46 +0600

    done with profile creation with user registration.

--- project4/settings.py
+++ project4/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'network',
+    #'network',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'network.apps.NetworkConfig'
 ]
 
 MIDDLEWARE = [
