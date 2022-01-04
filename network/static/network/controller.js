4e7b87dac222fe0ff9d93eed06f1d8c974e81f1b
rakinplaban <rakinshahriar54@gmail.com>
2022-01-04 21:57:55 +0600

    incomplete popup.

--- network/static/network/controller.js
+++ network/static/network/controller.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded",function(){
-    
+    document.querySelector('#NewPostForm').addEventListener('click', ()=> load_popup())
 })
+
+function load_popup(){
+    const popupfullbody = document.querySelector('body')
+    popupfullbody.ariaHidden = 'true';
+    const popupvar = document.createElement('div');
+    popupvar.setAttribute('id','popupform');
+    console.log("popup created!");
+    
+}
