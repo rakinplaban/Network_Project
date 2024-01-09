ab32200fbd2c830eba8e8e1d11c392055ba4a794
rakinplaban <rakinshahriar54@gmail.com>
2024-01-09 22:37:15 +0600

    fixed icon location

--- project4/settings.py
+++ project4/settings.py
@@ -131,9 +131,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/network/static/'
-MEDIA_URL = '/network/images/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'network','images')
+STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'network','static','images')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'network\static')
 ]
